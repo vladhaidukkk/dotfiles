a98fc317f9d020de9cdfb83fe2e4bc45af1a034d
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-11-23 23:42:30 +0200

    setup path and pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,40 @@
-# Clean up $HOME directory with XDG Base Directory specification
-# Use xdg-ninja utility to make it even better
-set -x XDG_DATA_HOME "$HOME/.local/share"
-set -x XDG_CONFIG_HOME "$HOME/.config"
-set -x XDG_STATE_HOME "$HOME/.local/state"
-set -x XDG_CACHE_HOME "$HOME/.cache"
+if status is-login
+    # Clean up $HOME directory with XDG Base Directory specification
+    # Use `xdg-ninja` utility to make it even better
+    set -x XDG_DATA_HOME "$HOME/.local/share"
+    set -x XDG_CONFIG_HOME "$HOME/.config"
+    set -x XDG_STATE_HOME "$HOME/.local/state"
+    set -x XDG_CACHE_HOME "$HOME/.cache"
+    set -x PYENV_ROOT $XDG_CONFIG_HOME/pyenv
 
-if status is-interactive
+    set -x PATH /Users/vladhaidukkk/Library/Application\ Support/JetBrains/Toolbox/scripts $PATH
+    set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+    set -x PATH $PYENV_ROOT/bin $PATH
+    # Created by `pipx`
+    set -x PATH $PATH /Users/vladhaidukkk/.local/bin
+
     set -x MANPAGER "col -bx | bat -l man -p"
 
+    pyenv init - | source
+end
+
+if status is-interactive
     # Dotfiles
     alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
-    # Change ls to exa
+    # ls -> exa
     alias ls="exa --group-directories-first"
     alias la="exa -a --group-directories-first"
     alias ll="exa -al --group-directories-first"
     alias lt="exa -aT --group-directories-first"
 
-    # Change rm to trash
+    # rm -> trash
     alias rm="trash"
 
-    # Change cat to bat
+    # cat -> bat
     alias cat="bat --paging=never"
 
-    # Change top to htop
+    # top -> htop
     alias top="htop"
 
     # Colorize grep output
@@ -31,9 +42,8 @@ if status is-interactive
     alias egrep="egrep --color=auto"
     alias fgrep="fgrep --color=auto"
 
-    # Python
-    alias python="python3"
-    alias pip="pip3"
+    # pyenv
+    alias pyvers="pyenv install -l | grep -E '^[[:blank:]]*[0-9]'"
 
     starship init fish | source
 end
