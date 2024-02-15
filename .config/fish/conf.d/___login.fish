e22bf39a0d5dd0b75d1019a1ecf10bc097a81d12
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-02-15 09:41:27 +0200

    move login setup from fish config to conf.d

---
+++ .config/fish/conf.d/___login.fish
@@ -0,0 +1,26 @@
+if status is-login
+    # Clean up $HOME directory with XDG Base Directory specification
+    # Use `xdg-ninja` utility to make it even better
+    set -x XDG_DATA_HOME "$HOME/.local/share"
+    set -x XDG_CONFIG_HOME "$HOME/.config"
+    set -x XDG_STATE_HOME "$HOME/.local/state"
+    set -x XDG_CACHE_HOME "$HOME/.cache"
+
+    set -x PATH /Users/vladhaidukkk/Library/Application\ Support/JetBrains/Toolbox/scripts $PATH
+    set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+
+    # Setup `pyenv`
+    set -x PYENV_ROOT $XDG_CONFIG_HOME/pyenv
+    set -x PATH $PYENV_ROOT/bin $PATH
+    pyenv init - | source
+
+    # Created by `pipx`
+    set -x PATH $PATH /Users/vladhaidukkk/.local/bin
+
+    # Setup `direnv`
+    direnv hook fish | source
+    set -x DIRENV_LOG_FORMAT ""
+
+    # Misc env vars
+    set -x MANPAGER "col -bx | bat -l man -p"
+end
