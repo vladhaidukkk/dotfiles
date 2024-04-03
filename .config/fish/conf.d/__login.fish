fedff9c4f1c60723b1346cd6734ab3751c3596b6
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-04-03 20:01:06 +0300

    clean up home directory

--- .config/fish/conf.d/__login.fish
+++ .config/fish/conf.d/__login.fish
@@ -6,11 +6,18 @@ if status is-login
     set -x XDG_STATE_HOME "$HOME/.local/state"
     set -x XDG_CACHE_HOME "$HOME/.cache"
 
+    set -x AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
+    set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
+    set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+    set -x IPYTHONDIR $XDG_CONFIG_HOME/ipython
+    set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
+    set -x PSQL_HISTORY $XDG_DATA_HOME/psql_history
+    set -x PYENV_ROOT $XDG_CONFIG_HOME/pyenv
+
     set -x PATH /Users/vladhaidukkk/Library/Application\ Support/JetBrains/Toolbox/scripts $PATH
     set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 
     # Setup `pyenv`
-    set -x PYENV_ROOT $XDG_CONFIG_HOME/pyenv
     set -x PATH $PYENV_ROOT/bin $PATH
     pyenv init - | source
 
@@ -18,8 +25,8 @@ if status is-login
     set -x PATH $PATH /Users/vladhaidukkk/.local/bin
 
     # Setup `direnv`
-    direnv hook fish | source
     set -x DIRENV_LOG_FORMAT ""
+    direnv hook fish | source
 
     # Setup `zoxide`
     zoxide init fish | source
