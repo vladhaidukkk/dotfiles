de759eb0da7b6b767cdba7f6aa141af4ff26db3d
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2026-02-20 15:37:16 +0100

    clean up fish config

--- .config/fish/conf.d/__login.fish
+++ .config/fish/conf.d/__login.fish
@@ -1,41 +1,22 @@
 if status is-login
     # Clean up $HOME directory with XDG Base Directory specification
-    # Use `xdg-ninja` utility to make it even better
-    set -x XDG_DATA_HOME "$HOME/.local/share"
-    set -x XDG_CONFIG_HOME "$HOME/.config"
-    set -x XDG_STATE_HOME "$HOME/.local/state"
-    set -x XDG_CACHE_HOME "$HOME/.cache"
+    # Use `xdg-ninja` utility to identify new entities to move
+    set -x XDG_DATA_HOME $HOME/.local/share
+    set -x XDG_CONFIG_HOME $HOME/.config
+    set -x XDG_STATE_HOME $HOME/.local/state
+    set -x XDG_CACHE_HOME $HOME/.cache
 
-    set -x AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
-    set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
     set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
     set -x IPYTHONDIR $XDG_CONFIG_HOME/ipython
-    set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
-    set -x PSQL_HISTORY $XDG_DATA_HOME/psql/psql_history
     set -x PYENV_ROOT $XDG_CONFIG_HOME/pyenv
 
-    set -x PATH /Users/vladhaidukkk/Library/Application\ Support/JetBrains/Toolbox/scripts $PATH
+    # Set up homebrew
     set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 
-    # Setup `pyenv`
+    # Set up pyenv
     set -x PATH $PYENV_ROOT/bin $PATH
-    pyenv init - | source
+    pyenv init - fish | source
 
-    # Created by `pipx`
-    set -x PATH $PATH /Users/vladhaidukkk/.local/bin
-
-    # Setup `jenv`
-    set -x PATH $HOME/.jenv/bin $PATH
-    jenv init - | source
-
-    # Setup `direnv`
-    set -x DIRENV_LOG_FORMAT ""
-    direnv hook fish | source
-
-    # Setup `zoxide`
+    # Set up zoxide (replaces cd)
     zoxide init --cmd cd fish | source
-
-    # Misc env vars
-    set -x EDITOR "vim"
-    set -x MANPAGER "col -bx | bat -l man -p"
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,40 +1,12 @@
 if status is-interactive
-    # Start SSH Agent
+    # Start SSH Agent (caches SSH keys to avoid repeated passphrase prompts)
     if not pgrep -f ssh-agent >/dev/null
         ssh-agent -c | source
     end
 
-    # Dotfiles
+    # Git for dotfiles management
     alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
-
-    # ls -> eza
-    alias ls="eza -1 --group-directories-first --icons"
-    alias la="eza -1a --group-directories-first --icons"
-    alias ll="eza -1al --bytes --no-user --no-time --group-directories-first --icons"
-    alias lt="eza -1a --tree --group-directories-first --icons"
 
     # rm -> trash
     alias rm="trash"
-
-    # cat -> bat
-    alias cat="bat --paging=never"
-
-    # top -> htop
-    alias top="htop"
-
-    # Colorize `grep` output
-    alias grep="grep --color=auto"
-    alias egrep="egrep --color=auto"
-    alias fgrep="fgrep --color=auto"
-
-    # pyenv
-    alias pyvers="pyenv install -l | grep -E '^[[:blank:]]*[0-9]'"
-
-    # tmuxinator
-    alias mux="tmuxinator"
-
-    # aws localstack
-    alias awslocal="aws --endpoint-url http://localhost:4566"
-
-    starship init fish | source
 end
